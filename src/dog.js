cdcb4881663cef7528e288c62f5d9296a55527e7
Matthew R <matthew@mjrouser.com>
2017-08-16 17:09:31 -0400

    added eslint config

--- src/dog.js
+++ src/dog.js
@@ -1,11 +1,11 @@
 class Dog {
-	constructor(name){
-		this.name = name
-	}
+  constructor(name) {
+    this.name = name
+  }
 
-	bark (){
-		return `Wah wah, I am ${this.name}`
-	}
+  bark() {
+    return `Wah wah, I am ${this.name}`
+  }
 }
 
 export default Dog
